fd425d234f3b6b5383df7d53f1801739a401defb
zaidejaz <zaidejaz521@gmail.com>
2023-08-13 16:01:38 +0500

    Getting Started

---
+++ src/components/SiderBar.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+import { Button } from './ui/button'
+
+const SiderBar = () => {
+    return (
+        <div className={"w-[15vw]"}>
+            <div className="space-y-4 py-4">
+                <div className="px-3 py-2">
+                    <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
+                        Discover
+                    </h2>
+                    <div className="space-y-1">
+                    <Button variant="secondary" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <polygon points="10 8 16 12 10 16 10 8" />
+              </svg>
+              Gallery
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <rect width="7" height="7" x="3" y="3" rx="1" />
+                <rect width="7" height="7" x="14" y="3" rx="1" />
+                <rect width="7" height="7" x="14" y="14" rx="1" />
+                <rect width="7" height="7" x="3" y="14" rx="1" />
+              </svg>
+              Albums
+            </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SiderBar
