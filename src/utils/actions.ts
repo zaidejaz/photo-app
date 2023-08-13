0af3309277455fc7d7329c98d8a9764991b2b3e9
zaidejaz <zaidejaz521@gmail.com>
2023-08-13 20:43:14 +0500

    Add to favourites

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -3,14 +3,10 @@ import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
 export async function setAsFavouriteAction(publicId:string, isFavourited:boolean){
     await cloudinary.v2.uploader.add_tag("favourite", [publicId]);
-    console.log("Added To Favourite")
-    console.log({ isFavourited })
     revalidatePath("/gallery")
 }
 
 export async function removeAsFavouriteAction(publicId:string, isFavourited:boolean){
     await cloudinary.v2.uploader.remove_tag("favourite", [publicId]);
-    console.log("Removed From Favourite");
-    console.log({ isFavourited })
     revalidatePath("/gallery")
 }
