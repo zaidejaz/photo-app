d5406fa54e687c1a2b50f82e1c9231d740cb09cb
zaidejaz <zaidejaz521@gmail.com>
2023-08-16 13:33:32 +0500

    Changes to Gallery

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,12 +1,11 @@
 "use server"
 import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
-export async function setAsFavouriteAction(publicId:string, isFavourited:boolean){
+
+export async function setAsFavouriteAction(publicId:string){
     await cloudinary.v2.uploader.add_tag("favourite", [publicId]);
-    revalidatePath("/gallery")
 }
 
-export async function removeAsFavouriteAction(publicId:string, isFavourited:boolean){
+export async function removeAsFavouriteAction(publicId:string){
     await cloudinary.v2.uploader.remove_tag("favourite", [publicId]);
-    revalidatePath("/gallery")
 }
