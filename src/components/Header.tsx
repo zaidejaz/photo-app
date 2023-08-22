8834303883aace7e53095da8ddd7a7a4b11eb8b9
zaidejaz <zaidejaz521@gmail.com>
2023-08-22 17:07:30 +0500

    Added Logo And Favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,23 @@ import React from "react";
 import { ModeToggle } from "./toggle-theme";
 import UploadBtn from "./UploadBtn";
 import MobileSiderBar from "./mobile-sidebar";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       <nav className="flex justify-between p-4 items-center border-b rounded-sm border-gray-300">
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-4">
           <MobileSiderBar className="lg:hidden" />
-          <h2 className="text-3xl font-semibold transition-colors">
+          <Link href={"/"}>
+          <div className="flex items-center space-x-2">
+          <Image src={"/logo.png"} width={50} height={50} alt="PicPerfect" />
+          <h2 className="text-3xl font-semibold transition-colors text-primary hidden md:flex">
             Photo App
           </h2>
+          </div>
+          </Link>
         </div>
         <div className="flex space-x-2 md:space-x-4">
           <UploadBtn />
--- src/components/SiderBar.tsx
+++ src/components/SiderBar.tsx
@@ -1,47 +1,53 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { CardStackIcon, HeartIcon, ImageIcon } from "@radix-ui/react-icons";
 
-const SiderBar = () => (
-  <div className="w-[18vw]">
-    <div className="space-y-4 py-4">
-      <div className="px-3 py-2">
-        <h2 className="mb-2 px-4 text-3xl font-bold tracking-tight">
-          Discover
-        </h2>
-        <div className="space-y-1">
-          <Link href={"/gallery"}>
-            <Button
-              variant={"ghost"}
-              className="w-full justify-start gap-2 items-center"
-            >
-              <ImageIcon width={20} height={20}/>
-              <span className="text-xl">Gallery</span>
-            </Button>
-          </Link>
-          <Link href={"/albums"}>
-            <Button
-              variant={"ghost"}
-              className="w-full justify-start gap-2 items-center"
-            >
-              <CardStackIcon width={20} height={20}/>
-              <span className="text-xl">Albums</span>
-            </Button>
-          </Link>
-          <Link href={"/favourites"}>
-            <Button
-              variant={"ghost"}
-              className="flex w-full justify-start gap-2 items-center"
-            >
-              <HeartIcon width={20} height={20}/>
-              <span className="text-xl">Favourites</span>
-            </Button>
-          </Link>
+const SiderBar = () => {
+  const [selected, setSelected] = useState("gallery");
+  return (
+    <div className="w-[18vw]">
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-3xl font-bold tracking-tight">
+            Discover
+          </h2>
+          <div className="space-y-1">
+            <Link href={"/gallery"}>
+              <Button
+                variant={selected === "gallery" ? "default" : "ghost"}
+                className="w-full justify-start gap-2 items-center"
+                onClick={() => setSelected("gallery")}
+              >
+                <ImageIcon width={20} height={20} />
+                <span className="text-xl">Gallery</span>
+              </Button>
+            </Link>
+            <Link href={"/albums"}>
+              <Button
+                variant={selected === "albums" ? "default" : "ghost"}
+                className="w-full justify-start gap-2 items-center"
+                onClick={() => setSelected("albums")}
+              >
+                <CardStackIcon width={20} height={20} />
+                <span className="text-xl">Albums</span>
+              </Button>
+            </Link>
+            <Link href={"/favourites"}>
+              <Button
+                variant={selected === "favourites" ? "default" : "ghost"}
+                className="flex w-full justify-start gap-2 items-center"
+                onClick={() => setSelected("favourites")}
+              >
+                <HeartIcon width={20} height={20} />
+                <span className="text-xl">Favourites</span>
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default SiderBar;
